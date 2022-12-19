5a9b89e6e19bed5335653028f62c42965d5b63ec
André Pedersen <andrped94@gmail.com>
2022-12-19 21:58:22 +0100

    fixed livermask processing - now it runs

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory
 import yaml
 from yaml.loader import SafeLoader
+from werkzeug.utils import secure_filename
+import tempfile
+import os
+import subprocess as sp
 
 
 app = Flask("SINTEF MIA Catalogue")
@@ -20,11 +24,34 @@ def application(app_name):
     return render_template("{}.jinja2".format(app_name.lower()), **{'app_name': app_name.capitalize()})
 
 
-# @app.route("process/<string:app_name>", methods=['GET', 'POST'])
-# def process(app_name):
-#     if request.method == 'POST':
-#         # Forward the uploaded data to the corresponding service
-#         APP_DICT[app_name].
+@app.route("/process", methods=["GET", "POST"])
+def process():
+    if request.method == "POST":
+        # create temporary directory - will automatically delete when exiting context manager
+        with tempfile.TemporaryDirectory() as dir_name:
+            # upload
+            f = request.files["file"]
+            curr_path = os.path.join(dir_name, secure_filename(f.filename))
+            f.save(curr_path)
+
+            # process
+            sp.check_call(["livermask", "--input", curr_path, "--output", os.path.join(dir_name, "prediction"), "--verbose"])
+
+            return send_from_directory(dir_name, "prediction-livermask.nii")
+
+        # return "file uploaded successfully"
+
+
+"""
+# @app.route("/process/<string:app_name>", methods=['GET', 'POST'])
+@app.route("/upload")
+def process(app_name):
+    if request.method == 'POST':
+        f = request.files["the_file"]
+        f.save("./{secure_filename(file.filename)")
+        # Forward the uploaded data to the corresponding service
+        # APP_DICT[app_name].
+"""
 
 
 def load_catalogue(catalogue_configuration_file: str='resources/catalogue.yaml') -> dict:
