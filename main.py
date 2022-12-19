8cc4a121fda5a3bebcd484cc0bb3506f74084a83
jpdefrutos <javier.perezdefrutos@sintef.no>
2022-12-19 15:35:49 +0100

    Added application page
    Added SINTEF logo

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ from flask import Flask, render_template
 import yaml
 from yaml.loader import SafeLoader
 
-app = Flask("SINTEF MedIA Catalogue")
+app = Flask("SINTEF MIA Catalogue")
+APP_DICT = dict()
 
 @app.route("/")
 def main():
     ctx = load_catalogue()
+    for app in ctx['apps']:
+        APP_DICT[app['name']] = app
     return render_template("catalogue.jinja2", title="Catalogue", **ctx)   # By default, Flask expects your templates in a templates/ directory
 
-@app.route("/app")
-def catalogue():
-    return render_template("application_template.jinja2")
+@app.route("/application/<string:app_name>")
+def application(app_name):
+    return render_template("{}.jinja2".format(app_name.lower()), **{'app_name': app_name.capitalize()})
+
+
+# @app.route("process/<string:app_name>", methods=['GET', 'POST'])
+# def process(app_name):
+#     if request.method == 'POST':
+#         # Forward the uploaded data to the corresponding service
+#         APP_DICT[app_name].
 
 
 def load_catalogue(catalogue_configuration_file: str='resources/catalogue.yaml') -> dict:
